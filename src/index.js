39e087e7a8f4f757a2ef3875ee5a1b6a622b57fb
slqpez <santiago.lopez16@udea.edu.co>
2021-02-12 19:54:56 -0500

    Some functions.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
 import UI from "./modules/UI.js"
 import Budget from "./modules/Budget.js"
 
-const name = document.querySelector("#name-input");
+
 
 function App(){
 
 }
 
 document.addEventListener("DOMContentLoaded",()=>{
-    let budget = prompt("Ingresa tu presupuesto: ")
-    UI.startBudget(budget);
+    const floatNumber = document.querySelector(".floating-number");
+    if(floatNumber.value===undefined){
+        let budget = Number(prompt("Ingresa tu presupuesto: "))
+        if(budget==="" ||isNaN(budget) || budget<=0){
+            window.location.reload();
+        }
+        else{
+            UI.startBudget(budget);
+            localStorage.setItem("floatNumber", budget)
+        }
+       
+    }
+
+    
+   
 })
 
 
-
+const btn = document.querySelector(".btn")
+btn.addEventListener("click",addItem)
+function addItem(e){
+    const name =document.querySelector("#name-input")
+    const cost =document.querySelector("#cost-input")
+    UI.showInList(name.value, cost.value)
+    e.preventDefault()
+}
